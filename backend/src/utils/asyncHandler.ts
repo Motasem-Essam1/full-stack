bd2fce52a3988283ba50df5619984f42e0d4c34d
toonbigdataking <motasem.essam.prototypedoteg.com@gmail.com>
2024-12-26 06:57:25 +0200

    first

--- backend/src/utils/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/asyncHandler.ts
-import { Request, Response, NextFunction } from 'express';
-
-const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) => 
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-
-export default asyncHandler;
