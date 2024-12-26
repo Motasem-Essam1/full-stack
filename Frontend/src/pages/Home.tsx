13b40289e811ad0f8091df2aae05b0095b798dc6
toonbigdataking <motasem.essam.prototypedoteg.com@gmail.com>
2024-12-26 08:58:58 +0200

    first

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Full-Stack App!</h1>
+      <ProductList />
+    </div>
+  );
+};
+
+export default Home;
+
+
